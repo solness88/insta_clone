73be3d5c0fce605b5a2a98ffcf8f892c47e919f4
hirokazu kobayashi <solness.stf@gmail.com>
2020-11-13 23:58:03 +0900

    インスタクローンDRY実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -24,6 +24,13 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました"
+  end
+  def confirm
+    @post = Post.new(post_params)
   end
   private
   def post_params

1b1e869cc3a7273623582344cb6bab5a03361fd5
hirokazu kobayashi <solness.stf@gmail.com>
2020-11-14 22:38:55 +0900

    ログイン・ログアウト機能実装中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :posts do
     collection do
       post :confirm
     end
   end
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
